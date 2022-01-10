21f26d90e80b354f334e202088ab15671713381c
Martin Kobetic <mkobetic@gmail.com>
2022-01-10 15:51:14 -0500

    encrypt/decrypt

--- packages/messaging/src/crypto.ts
+++ packages/messaging/src/crypto.ts
@@ -1,4 +1,9 @@
 import * as secp from "@noble/secp256k1";
+import * as crypto from "crypto";
+
+export const AESKeySize = 32;
+export const KDFSaltSize = 32;
+export const AESGCMNonceSize = 12;
 
 export class Signature {
     bytes: Uint8Array;
@@ -11,8 +16,12 @@ export class Signature {
 
 export class PrivateKey {
     bytes: Uint8Array;
-    constructor () {
-        this.bytes = secp.utils.randomPrivateKey()
+    constructor (bytes: Uint8Array) { this.bytes = bytes };
+    static generate(): PrivateKey {
+        return new PrivateKey(secp.utils.randomPrivateKey())
+    };
+    static fromBytes(bytes: Uint8Array): PrivateKey {
+        return new PrivateKey(bytes)
     };
     sign(digest: Uint8Array):Promise<Signature> {
         return secp.sign(digest, this.bytes, {recovered: true})
@@ -25,27 +34,87 @@ export class PrivateKey {
                 pub.signature = signature;
                 return pub })
     }
+    sharedSecret(peer: PublicKey): Uint8Array {
+        return secp.getSharedSecret(this.bytes, peer.bytes, false)
+    }
+    encrypt(plain: Uint8Array, peer: PublicKey, additionalData?: Uint8Array):Promise<[Uint8Array,Uint8Array,Uint8Array]> {
+        const secret = this.sharedSecret(peer)
+        return encrypt(plain, secret, additionalData)
+    }
+    decrypt(encrypted: Uint8Array, peer: PublicKey, salt: Uint8Array, nonce: Uint8Array, additionalData?: Uint8Array): Promise<Uint8Array> {
+        const secret = this.sharedSecret(peer)
+        return decrypt(encrypted, secret, salt, nonce, additionalData)
+    }
 }
 
 export class PublicKey {
     bytes: Uint8Array;
     signature?: Signature;
-    constructor (pri: PrivateKey) {
-        this.bytes = secp.getPublicKey(pri.bytes)
+    constructor(bytes: Uint8Array, signature?: Signature) {
+        this.bytes = bytes;
+        this.signature = signature
+    }
+    static fromPrivateKey(pri: PrivateKey): PublicKey {
+      return new PublicKey(secp.getPublicKey(pri.bytes))
     };
+    static fromBytes(bytes: Uint8Array): PublicKey {
+        return new PublicKey(bytes)
+    }
     verify(signature: Signature, digest: Uint8Array):boolean {
         return secp.verify(signature.bytes, digest, this.bytes)
     }
     verifyKey(pub: PublicKey):Promise<boolean> {
-        return pub.signature === undefined ?
-            Promise.resolve(false) :
-            secp.utils.sha256(pub.bytes)
-                .then(digest => this.verify(pub.signature, digest))
+        if (typeof pub.signature === undefined) { return Promise.resolve(false) };
+        return secp.utils.sha256(pub.bytes)
+            .then(digest => this.verify(pub.signature, digest))
     }
 }
 
 export function generateKeys(): [PrivateKey, PublicKey] {
-    const pri = new PrivateKey();
-    const pub = new PublicKey(pri);
+    const pri = PrivateKey.generate();
+    const pub = PublicKey.fromPrivateKey(pri);
     return [pri, pub]
+}
+
+// Derive AES-256-GCM key from a shared secret and salt.
+// Returns crypto.CryptoKey suitable for the encrypt/decrypt API
+function hkdf(secret: Uint8Array, salt: Uint8Array):Promise<crypto.CryptoKey> {
+    return crypto.subtle.importKey("raw", secret, "HKDF", false, ["deriveKey"])
+    .then(key => crypto.subtle.deriveKey(
+            {name: "HKDF", hash: "SHA256", salt: salt},
+            key,
+            {name: "AES-GCM", length: 256},
+            false,
+            ["encrypt", "decrypt"]) )
+}
+
+function encrypt(plain: Uint8Array, secret: Uint8Array, additionalData?: Uint8Array):Promise<[Uint8Array,Uint8Array,Uint8Array]> {
+    var salt = new Uint8Array(KDFSaltSize);
+    crypto.getRandomValues(salt);
+    var nonce = new Uint8Array(AESGCMNonceSize);
+    crypto.getRandomValues(nonce);
+    hkdf(secret, salt)
+    .then(key => {
+        var spec = {
+            name: "AES-GCM",
+            iv: nonce };
+        if (additionalData) {
+            spec.additionalData = additionalData
+        }
+        return crypto.subtle.encrypt(spec, key, plain)})
+    .then( encrypted => [ encrypted, salt, nonce ])
+}
+
+function decrypt(encrypted: Uint8Array, secret: Uint8Array, salt: Uint8Array, nonce: Uint8Array, additionalData?: Uint8Array): Promise<Uint8Array> {
+    return hkdf(secret, salt)
+        .then(key => {
+            var spec = {
+                name: "AES-GCM",
+                iv: nonce
+            };
+            if (additionalData) {
+                spec.additionalData = additionalData
+            };
+            return crypto.subtle.decrypt(spec, key, encrypted)
+        })
 }
--- packages/messaging/test/index.ts
+++ packages/messaging/test/index.ts
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import { TextEncoder, TextDecoder } from 'util';
 import * as crypto from '../src/crypto';
 
 describe('Testing', function() {
@@ -10,9 +11,19 @@ describe('Testing', function() {
 
 describe('Crypto', function() {
   it('sign keys and verify signatures', async function() {
-    var [iPri, iPub] = crypto.generateKeys()
-    var [_, pPub] = crypto.generateKeys()
-    await iPri.signKey(pPub)
-    assert.ok(await iPub.verifyKey(pPub))
+    var [iPri, iPub] = crypto.generateKeys();
+    var [_, pPub] = crypto.generateKeys();
+    await iPri.signKey(pPub);
+    assert.ok(await iPub.verifyKey(pPub));
   });
+  it('encrypts and decrypts messages', async function() {
+    var [aPri, aPub] = crypto.generateKeys();
+    var [bPri, bPub] = crypto.generateKeys();
+    var msg1 = "Yo!";
+    var decrypted = new TextEncoder().encode(msg1);
+    var [encrypted, salt, nonce] = await aPri.encrypt(decrypted,bPub);
+    decrypted = await bPri.decrypt(encrypted, aPub, salt, nonce);
+    var msg2 = new TextDecoder().decode(decrypted);
+    assert.equal(msg2, msg1);
+  })
 });
