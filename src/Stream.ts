712573bb21a8e587d9814c20a599789a382b09bb
Martin Kobetic <mkobetic@gmail.com>
2022-02-02 22:16:33 -0500

    comments

--- src/Stream.ts
+++ src/Stream.ts
@@ -2,12 +2,14 @@ import { WakuMessage } from 'js-waku'
 import { Message } from '.'
 import Client from './Client'
 
+// Stream implements an Asynchronous Iterable over messages received from a topic.
+// As such can be used with constructs like for-await-of, yield*, array destructing, etc.
 export default class Stream {
-  messages: WakuMessage[]
-  resolvers: ((value: IteratorResult<Message>) => void)[]
+  messages: Message[] // queue of incoming Waku messages
+  resolvers: ((value: IteratorResult<Message>) => void)[] // queue of already pending Promises
   topic: string
   client: Client
-  callback: (wakuMsg: WakuMessage) => void
+  callback: (wakuMsg: WakuMessage) => void // caches the callback so that it can be properly deregistered in Waku
 
   constructor(client: Client, topic: string) {
     this.messages = []
@@ -18,21 +20,29 @@ export default class Stream {
       if (!wakuMsg.payload) {
         return
       }
+      const msg = await Message.decode(this.client.keys, wakuMsg.payload)
+      // is there a Promise already pending?
       const resolver = this.resolvers.pop()
       if (resolver) {
-        const msg = await Message.decode(this.client.keys, wakuMsg.payload)
+        // yes, resolve it
         resolver({ value: msg })
       } else {
-        this.messages.unshift(wakuMsg)
+        // no, push the message into the queue
+        this.messages.unshift(msg)
       }
     }
     client.waku.relay.addObserver(this.callback, [topic])
   }
 
+  // To make Stream proper Async Iterable
   [Symbol.asyncIterator](): AsyncIterableIterator<Message> {
     return this
   }
 
+  // return should be called if the interpreter detects that the stream won't be used anymore,
+  // e.g. a for/of loop was exited via a break. It can also be called explicitly.
+  // https://tc39.es/ecma262/#table-iterator-interface-optional-properties
+  // Note that this means the Stream will be closed after it was used in a for-await-of or yield* or similar.
   async return(): Promise<IteratorResult<Message>> {
     this.client.waku.relay.deleteObserver(this.callback, [this.topic])
     this.resolvers.forEach((resolve) =>
@@ -41,13 +51,17 @@ export default class Stream {
     return { value: undefined, done: true }
   }
 
+  // To make Stream proper Async Iterator
+  // Note that next() will still provide whatever messages were already pending
+  // even after the stream was closed via return().
   next(): Promise<IteratorResult<Message>> {
-    const wakuMsg = this.messages.pop()
-    if (wakuMsg?.payload) {
-      return Message.decode(this.client.keys, wakuMsg.payload).then((msg) => {
-        return { value: msg }
-      })
+    // Is there a message already pending?
+    const msg = this.messages.pop()
+    if (msg) {
+      // yes, return resolved promise
+      return Promise.resolve({ value: msg })
     }
+    // otherwise return empty Promise and queue its resolver
     return new Promise((resolve) => this.resolvers.unshift(resolve))
   }
 }
