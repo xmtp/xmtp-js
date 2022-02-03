662a270fa1b925c4a55d405eeae2db4b131227af
Steven Normore <snormore@gmail.com>
2022-02-03 18:30:00 -0500

    Expose Stream type through package index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import {
   PrivateKey,
   PrivateKeyBundle,
 } from '../src/crypto'
+import Stream from './Stream'
 import Client from './Client'
 
 export {
   Client,
+  Stream,
   Message,
   PublicKey,
   PublicKeyBundle,
