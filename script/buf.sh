95760fbd99248a0354a093265f37a232e58826cc
Nick Molnar <nmolnar@brex.com>
2022-02-28 14:38:42 -0800

    build: use npm friendly path

--- script/buf.sh
+++ script/buf.sh
@@ -13,13 +13,10 @@ fi
 # Use your desired buf version
 BUF_VERSION=1.0.0
 # Putting this in a location likely to already be in the $PATH
-BIN_DIR=$HOME/bin
-echo "Writing to $BIN_DIR"
-mkdir -p $BIN_DIR
+BIN_DIR="$npm_config_prefix/bin"
 
 URL="https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-$(uname -s)-$(uname -m)"
 echo "Downloading buf from $URL"
-echo $PATH
 
 curl -sSL $URL -o "$BIN_DIR/buf"
 chmod +x "$BIN_DIR/buf"
