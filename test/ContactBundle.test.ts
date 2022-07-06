962d8200e20fa29af88b6cc085ecf6a51d69f6eb
Jazz Turner-Baggs <jazz@xmtp.com>
2022-07-06 13:10:57 -0700

    fix: refactor await

--- test/ContactBundle.test.ts
+++ test/ContactBundle.test.ts
@@ -5,9 +5,9 @@ import { PrivateKeyBundle } from '../src'
 
 describe('ContactBundles', function () {
   it('roundtrip', async function () {
-    const priv = PrivateKeyBundle.generate()
+    const priv = await PrivateKeyBundle.generate()
 
-    const cb1 = new ContactBundle((await priv).getPublicKeyBundle())
+    const cb1 = new ContactBundle(priv.getPublicKeyBundle())
     const bytes1 = cb1.toBytes()
 
     const cb2 = ContactBundle.fromBytes(bytes1)
