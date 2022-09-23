d7c54b5127c4031a5f403a69b8a9f8b02ff0a195
Elise Alix <elise@xmtp.com>
2022-09-23 13:49:30 -0400

    feat: only use localstorage in tests

--- src/store/LocalStorageStore.ts
+++ src/store/LocalStorageStore.ts
@@ -1,7 +1,9 @@
 // This will create a global localStorage object on Node.js for use in tests
-// If we want to save some bytes from the bundle, we can have Webpack replace this with an empty module for the browser
-import 'node-localstorage/register'
 import { Store } from './Store'
+
+if (isDevEnvironment()) {
+  require('node-localstorage/register')
+}
 
 const KEY_PREFIX = '/xmtp/'
 const ENCODING = 'binary'
@@ -16,11 +18,18 @@ export default class LocalStorageStore implements Store {
   }
 
   async get(key: string): Promise<Buffer | null> {
+    if (isDevEnvironment()) return null
     const storedString = localStorage.getItem(this.keyPrefix + key)
     return storedString === null ? null : Buffer.from(storedString, ENCODING)
   }
 
   async set(key: string, value: Buffer): Promise<void> {
+    if (isDevEnvironment()) return
     return localStorage.setItem(this.keyPrefix + key, value.toString(ENCODING))
   }
+}
+
+function isDevEnvironment(): boolean {
+  console.log('isDev = ' + process.env.NODE_ENV !== 'production')
+  return process.env.NODE_ENV !== 'production'
 }
