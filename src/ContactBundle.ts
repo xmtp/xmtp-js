f9653e26567dd66143facdecbf32653cd2aba89e
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-10-05 12:16:04 -0400

    fix: encode bundle correctly

--- src/ContactBundle.ts
+++ src/ContactBundle.ts
@@ -1,7 +1,7 @@
 import { contact, publicKey } from '@xmtp/proto'
 import { PublicKeyBundle, SignedPublicKeyBundle } from './crypto'
 
-// This is the primary function for reading contact bundles off the wire.
+// Decodes contact bundles from the contact topic.
 export function decodeContactBundle(
   bytes: Uint8Array
 ): PublicKeyBundle | SignedPublicKeyBundle {
@@ -19,4 +19,21 @@ export function decodeContactBundle(
     return new SignedPublicKeyBundle(cb.v2.keyBundle)
   }
   throw new Error('unknown or invalid contact bundle')
+}
+
+// Encodes public key bundle for the contact topic.
+export function encodeContactBundle(
+  bundle: PublicKeyBundle | SignedPublicKeyBundle
+): Uint8Array {
+  if (bundle instanceof PublicKeyBundle) {
+    return contact.ContactBundle.encode({
+      v1: { keyBundle: bundle },
+      v2: undefined,
+    }).finish()
+  } else {
+    return contact.ContactBundle.encode({
+      v1: undefined,
+      v2: { keyBundle: bundle },
+    }).finish()
+  }
 }
--- test/ContactBundle.test.ts
+++ test/ContactBundle.test.ts
@@ -1,11 +1,10 @@
 import * as assert from 'assert'
-import { decodeContactBundle } from '../src/ContactBundle'
+import { decodeContactBundle, encodeContactBundle } from '../src/ContactBundle'
 import {
   PrivateKeyBundleV1,
   PrivateKeyBundleV2,
   PublicKeyBundle,
   SignedPublicKeyBundle,
-  PrivateKey,
 } from '../src'
 import { newWallet } from './helpers'
 
@@ -13,24 +12,18 @@ describe('ContactBundles', function () {
   it('roundtrip', async function () {
     const priv = await PrivateKeyBundleV1.generate()
     const pub = priv.getPublicKeyBundle()
-    let bytes = pub.toBytes()
+    let bytes = encodeContactBundle(pub)
     const cb = decodeContactBundle(bytes)
     expect(cb).toBeInstanceOf(PublicKeyBundle)
     assert.ok(pub.equals(cb as PublicKeyBundle))
-
-    const bytes2 = cb.toBytes()
-    assert.deepEqual(bytes, bytes2)
   })
   it('roundtrip v2', async function () {
     const wallet = newWallet()
     const priv = await PrivateKeyBundleV2.generate(wallet)
     const pub = priv.getPublicKeyBundle()
-    let bytes = pub.toBytes()
+    let bytes = encodeContactBundle(pub)
     const cb = decodeContactBundle(bytes)
     expect(cb).toBeInstanceOf(SignedPublicKeyBundle)
     assert.ok(pub.equals(cb as SignedPublicKeyBundle))
-
-    const bytes2 = cb.toBytes()
-    assert.deepEqual(bytes, bytes2)
   })
 })
