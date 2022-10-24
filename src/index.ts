03c59c347060d5b4e712873ab07b400c614fe612
Nicholas Molnar <nicholas@xmtp.com>
2022-10-24 14:49:18 -0700

    fix: update main package exports

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,30 @@
-import { Message, DecodedMessage } from './Message'
-import {
+export { Message, DecodedMessage } from './Message'
+export {
   PublicKey,
   PublicKeyBundle,
   SignedPublicKeyBundle,
   PrivateKey,
   PrivateKeyBundle,
 } from './crypto'
-import Stream from './Stream'
-import Client, {
+export { default as Stream } from './Stream'
+export {
+  default as Client,
   ClientOptions,
   ListMessagesOptions,
   SendOptions,
   Compression,
 } from './Client'
-import {
-  Conversations,
-  Conversation,
-  ConversationV1,
-  ConversationV2,
-} from './conversations'
-import {
+export { Conversations, Conversation } from './conversations'
+export {
   ContentTypeId,
   ContentCodec,
   EncodedContent,
   ContentTypeFallback,
 } from './MessageContent'
-import { TextCodec, ContentTypeText } from './codecs/Text'
-import {
+export { TextCodec, ContentTypeText } from './codecs/Text'
+export {
   Composite,
   CompositeCodec,
   ContentTypeComposite,
 } from './codecs/Composite'
-import { SortDirection } from './ApiClient'
-
-export {
-  Client,
-  Conversation,
-  ConversationV1,
-  ConversationV2,
-  Conversations,
-  ClientOptions,
-  DecodedMessage,
-  ListMessagesOptions,
-  Message,
-  PrivateKey,
-  PrivateKeyBundle,
-  PublicKey,
-  PublicKeyBundle,
-  SignedPublicKeyBundle,
-  Stream,
-  ContentTypeId,
-  ContentCodec,
-  EncodedContent,
-  TextCodec,
-  ContentTypeText,
-  ContentTypeFallback,
-  SendOptions,
-  Compression,
-  Composite,
-  CompositeCodec,
-  ContentTypeComposite,
-  SortDirection,
-}
+export { SortDirection } from './ApiClient'
