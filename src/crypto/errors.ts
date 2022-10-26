bbc28bb5156620147482bfb70ddcc7979d55a86d
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-10-26 14:12:55 -0400

    fix: rename parameter

--- src/crypto/errors.ts
+++ src/crypto/errors.ts
@@ -2,7 +2,9 @@ import { PublicKey, SignedPublicKey } from './PublicKey'
 import { bytesToHex } from './utils'
 
 export class NoMatchingPreKeyError extends Error {
-  constructor(peer: PublicKey | SignedPublicKey) {
-    super(`no pre-key matches: ${bytesToHex(peer.secp256k1Uncompressed.bytes)}`)
+  constructor(preKey: PublicKey | SignedPublicKey) {
+    super(
+      `no pre-key matches: ${bytesToHex(preKey.secp256k1Uncompressed.bytes)}`
+    )
   }
 }
