b0675c523b9277a665148fcaaeecaf20e21124ec
Jazz Turner-Baggs <jazz@xmtp.com>
2022-11-30 10:25:37 -0800

    Jazzz/memo integration (#229)
    * export signatures and signedPublicKeys
    
    * add signBytes functionality to client
    
    * expose decodedContent
    
    * expose apiUrls so that apps can access type def
    
    * review fixups

--- src/Client.ts
+++ src/Client.ts
@@ -3,6 +3,7 @@ import {
   SignedPublicKeyBundle,
   PrivateKeyBundleV1,
   PrivateKeyBundleV2,
+  Signature,
 } from './crypto'
 import {
   buildUserContactTopic,
@@ -417,6 +418,10 @@ export default class Client {
       ),
       mapper
     )
+  }
+
+  async signBytes(bytes: Uint8Array): Promise<Signature> {
+    return this.keys.identityKey.sign(bytes)
   }
 }
 
