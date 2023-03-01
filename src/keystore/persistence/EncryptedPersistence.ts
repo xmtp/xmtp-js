e3111bb92b2e525bf114e77e147e79efd85e9531
Nicholas Molnar <nicholas@xmtp.com>
2023-03-01 11:56:19 -0800

    fix: address comments

--- src/keystore/persistence/EncryptedPersistence.ts
+++ src/keystore/persistence/EncryptedPersistence.ts
@@ -1,17 +1,21 @@
 import { Persistence } from './interface'
 import eccrypto, { Ecies } from 'eccrypto'
 
+const IV_LENGTH = 16
+const EPHEMERAL_PUBLIC_KEY_LENGTH = 65
+const MAC_LENGTH = 32
+
 const assertEciesLengths = (ecies: Ecies): void => {
-  if (ecies.iv.length !== 16) {
+  if (ecies.iv.length !== IV_LENGTH) {
     throw new Error('Invalid iv length')
   }
-  if (ecies.ephemPublicKey.length !== 65) {
+  if (ecies.ephemPublicKey.length !== EPHEMERAL_PUBLIC_KEY_LENGTH) {
     throw new Error('Invalid ephemPublicKey length')
   }
-  if (ecies.ciphertext.length < 1) {
+  if (ecies.ciphertext.length < 1 || ecies.ciphertext.length % 16 !== 0) {
     throw new Error('Invalid ciphertext length')
   }
-  if (ecies.mac.length !== 32) {
+  if (ecies.mac.length !== MAC_LENGTH) {
     throw new Error('Invalid mac length')
   }
 }
@@ -23,16 +27,22 @@ const serializeEcies = (ecies: Ecies): Uint8Array => {
     iv.length + ephemPublicKey.length + ciphertext.length + mac.length
   )
   result.set(iv, 0)
-  result.set(ephemPublicKey, iv.length)
-  result.set(ciphertext, iv.length + ephemPublicKey.length)
-  result.set(mac, iv.length + ephemPublicKey.length + ciphertext.length)
+  result.set(ephemPublicKey, IV_LENGTH)
+  result.set(ciphertext, IV_LENGTH + EPHEMERAL_PUBLIC_KEY_LENGTH)
+  result.set(mac, IV_LENGTH + EPHEMERAL_PUBLIC_KEY_LENGTH + ciphertext.length)
   return result
 }
 
 const deserializeEcies = (data: Uint8Array): Ecies => {
-  const iv = data.slice(0, 16)
-  const ephemPublicKey = data.slice(16, 81)
-  const ciphertext = data.slice(81, data.length - 32)
+  const iv = data.slice(0, IV_LENGTH)
+  const ephemPublicKey = data.slice(
+    IV_LENGTH,
+    IV_LENGTH + EPHEMERAL_PUBLIC_KEY_LENGTH
+  )
+  const ciphertext = data.slice(
+    IV_LENGTH + EPHEMERAL_PUBLIC_KEY_LENGTH,
+    data.length - 32
+  )
   const mac = data.slice(data.length - 32, data.length)
   return {
     iv: Buffer.from(iv),
