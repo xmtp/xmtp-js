f4d696b1e2920def1ca90d9704d8f601b4bf918f
Michael Xu <michaelx@xmtp.com>
2023-03-01 13:31:01 -0800

    test: added negative test cases to EncryptedPersistence

--- test/keystore/persistence/EncryptedPersistence.test.ts
+++ test/keystore/persistence/EncryptedPersistence.test.ts
@@ -67,4 +67,67 @@ describe('EncryptedPersistence', () => {
     ])
     expect(rawResult1).not.toEqual(rawResult2)
   })
+
+  it('catches garbage values', async () => {
+    const data = getRandomValues(new Uint8Array(128))
+    const persistence = new LocalStoragePersistence()
+    const encryptedPersistence = new EncryptedPersistence(
+      persistence,
+      privateKey
+    )
+
+    // Set an unencrypted value of 'garbage' as bytes
+    await persistence.setItem(
+      TEST_KEY,
+      new Uint8Array([103, 97, 114, 98, 97, 103, 101])
+    )
+    // Expect an error if the ciphertext is tampered with
+    await expect(encryptedPersistence.getItem(TEST_KEY)).rejects.toThrow(
+      'Invalid data length'
+    )
+  })
+
+  it('detects bad mac', async () => {
+    const data = getRandomValues(new Uint8Array(128))
+    const persistence = new LocalStoragePersistence()
+    const encryptedPersistence = new EncryptedPersistence(
+      persistence,
+      privateKey
+    )
+
+    // Write the value with encryption
+    await encryptedPersistence.setItem(TEST_KEY, data)
+
+    // Read the raw result, change one byte, write it back
+    const rawResult = await persistence.getItem(TEST_KEY)!
+    rawResult![7] += 1
+    await persistence.setItem(TEST_KEY, rawResult!)
+
+    // Expect an error if the ciphertext is tampered with
+    await expect(encryptedPersistence.getItem(TEST_KEY)).rejects.toThrow(
+      'Bad MAC'
+    )
+  })
+
+  it('detects length modified ciphertext', async () => {
+    const data = getRandomValues(new Uint8Array(128))
+    const persistence = new LocalStoragePersistence()
+    const encryptedPersistence = new EncryptedPersistence(
+      persistence,
+      privateKey
+    )
+
+    await encryptedPersistence.setItem(TEST_KEY, data)
+    // Read the raw result, change one byte, write it back
+    const rawResult = await persistence.getItem(TEST_KEY)!
+    // Add a byte to the rawResult
+    const newRawResult = new Uint8Array(rawResult!.length + 1)
+    newRawResult.set(rawResult!)
+    await persistence.setItem(TEST_KEY, newRawResult)
+
+    // Expect an error if the ciphertext is tampered with
+    await expect(encryptedPersistence.getItem(TEST_KEY)).rejects.toThrow(
+      'Invalid data length'
+    )
+  })
 })
