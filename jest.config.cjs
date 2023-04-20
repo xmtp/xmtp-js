c3d12920bc7ace465fbded9b18204dada8ee743b
Ry Racherbaumer <ry@xmtp.com>
2023-04-20 09:20:31 -0500

    build: update build process
    BREAKING CHANGE: The NPM package no longer contains a mirror of the `/src`
    directory. Therefore, any consumers of this package that rely on importing
    from the `/dist` folder will break.
    
    After these changes have merged, all externally available exports must be
    explicitly defined in `src/index.ts`.

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,7 +11,5 @@ module.exports = {
         tsconfig: 'tsconfig.json',
       },
     ],
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      './config/fileTransformer.js',
   },
 }
---
+++ tsup.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'tsup'
+import externalGlobal from 'esbuild-plugin-external-global'
+import { Plugin } from 'esbuild'
+
+export default defineConfig((options) => {
+  const esbuildPlugins: Plugin[] = []
+
+  if (options.platform === 'browser') {
+    esbuildPlugins.push(
+      externalGlobal.externalGlobalPlugin({
+        crypto: '{ webcrypto: window.crypto }',
+      })
+    )
+  }
+
+  const isBench = options.entry?.[0] === 'bench/index.ts'
+
+  return {
+    entry: options.entry ?? ['src/index.ts'],
+    outDir:
+      options.outDir ?? (options.platform === 'browser' ? 'dist/web' : 'dist'),
+    splitting: false,
+    sourcemap: !isBench,
+    treeshake: true,
+    clean: true,
+    bundle: true,
+    platform: options.platform ?? 'node',
+    minify: isBench ? false : options.platform === 'browser',
+    dts: isBench ? false : options.platform !== 'browser',
+    format:
+      options.format ??
+      (options.platform === 'browser' ? ['esm'] : ['esm', 'cjs']),
+    esbuildPlugins,
+  }
+})
