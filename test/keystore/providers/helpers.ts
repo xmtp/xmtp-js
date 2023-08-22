f49c83915fc8d02b99d3dfd79bcef4906730d13d
Nicholas Molnar <nicholas@xmtp.com>
2023-08-21 18:52:53 -0700

    test: handle new provider options

--- test/keystore/providers/helpers.ts
+++ test/keystore/providers/helpers.ts
@@ -1,11 +1,13 @@
 import { LocalStoragePersistence } from '../../../src'
+import { KeystoreProviderOptions } from '../../../src/keystore/providers'
 
-export const testProviderOptions = (
+export const testProviderOptions = ({
   privateKeyOverride = undefined,
   persistConversations = false,
-  basePersistence = new LocalStoragePersistence()
-) => ({
-  env: 'local' as const,
+  basePersistence = new LocalStoragePersistence(),
+  env = 'local' as const,
+}: Partial<KeystoreProviderOptions>) => ({
+  env,
   persistConversations,
   privateKeyOverride,
   basePersistence,
