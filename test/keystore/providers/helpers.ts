d1ff3ebd7713f7af47675d07e1af175a79a69fb8
Nicholas Molnar <nicholas@xmtp.com>
2023-08-21 18:40:08 -0700

    test: add base persistence

--- test/keystore/providers/helpers.ts
+++ test/keystore/providers/helpers.ts
@@ -1,8 +1,13 @@
+import { LocalStoragePersistence } from '../../../src'
+
 export const testProviderOptions = (
   privateKeyOverride = undefined,
-  persistConversations = false
+  persistConversations = false,
+  basePersistence = new LocalStoragePersistence()
 ) => ({
   env: 'local' as const,
   persistConversations,
   privateKeyOverride,
+  basePersistence,
+  disablePersistenceEncryption: false,
 })
