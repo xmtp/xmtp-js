1b588b68dba64bcb9dd81dccb0feae71135b1985
Naomi Plasterer <naomi@xmtp.com>
2023-09-14 20:09:58 -0700

    fix the test

--- packages/content-type-read-receipt/src/ReadReceipt.test.ts
+++ packages/content-type-read-receipt/src/ReadReceipt.test.ts
@@ -44,7 +44,7 @@ describe("ReadReceiptContentType", () => {
     expect(messages.length).toBe(1);
 
     const readReceiptMessage = messages[0];
-    const messageContent = readReceiptMessage.content as ReadReceipt;
-    expect(messageContent.timestamp).toBe(dateString);
+    const messageContent = readReceiptMessage.contentType;
+    expect(messageContent.typeId).toBe("readReceipt");
   });
 });
