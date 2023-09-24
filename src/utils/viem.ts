9ef61812defe50d784904fac1ffc0cfc5c244860
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2023-09-23 23:48:21 -0400

    test: manually create wallet client

--- src/utils/viem.ts
+++ src/utils/viem.ts
@@ -1,6 +1,5 @@
 import type { WalletClient } from 'viem'
 import { Signer } from '../types/Signer'
-import { providers } from 'ethers'
 
 export function getSigner(wallet: Signer | WalletClient | null): Signer | null {
   if (!wallet) {
@@ -9,7 +8,7 @@ export function getSigner(wallet: Signer | WalletClient | null): Signer | null {
   if (isWalletClient(wallet)) {
     return convertWalletClientToSigner(wallet)
   }
-  if (!(typeof wallet.getAddress !== 'function')) {
+  if (typeof wallet.getAddress !== 'function') {
     throw new Error('Unknown wallet type')
   }
   return wallet
@@ -19,23 +18,19 @@ function isWalletClient(wallet: Signer | WalletClient): wallet is WalletClient {
   return 'type' in wallet && wallet.type === 'walletClient'
 }
 
-// Borrowed from https://wagmi.sh/react/ethers-adapters
 export function convertWalletClientToSigner(
   walletClient: WalletClient
 ): Signer {
-  const { account, chain, transport } = walletClient
-
-  if (!account || !chain) {
+  const { account } = walletClient
+  if (!account || !account.address) {
     throw new Error('WalletClient is not configured')
   }
 
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
+  return {
+    getAddress: async () => account.address,
+    signMessage: async (message: string) => {
+      const signature = await walletClient.signMessage({ message, account })
+      return signature
+    },
   }
-
-  const provider = new providers.Web3Provider(transport, network)
-  const signer = provider.getSigner(account.address)
-  return signer
 }
