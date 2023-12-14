4b51dd35d24a3db3348312b79677be628adf3a22
Ry Racherbaumer <ry@xmtp.com>
2023-12-14 17:09:37 -0600

    refactor: update crypto exports

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -1,16 +1,4 @@
-/**
- * This file is necessary to ensure that the crypto library is available
- * in node and the browser
- */
-
 // eslint-disable-next-line no-restricted-syntax
-import { webcrypto as nodeCrypto } from 'crypto'
-
-const webcrypto =
-  typeof globalThis === 'object' && 'crypto' in globalThis
-    ? (globalThis.crypto as nodeCrypto.Crypto)
-    : undefined
-
-const crypto = webcrypto ?? nodeCrypto
-
+import { webcrypto } from 'crypto'
+const crypto = webcrypto
 export default crypto
