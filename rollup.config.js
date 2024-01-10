4b0c13863a8089657c032e0bf0c0c7ecfeeced2d
Ry Racherbaumer <ry@xmtp.com>
2024-01-10 14:17:28 -0600

    fix: replace `tsup` with `rollup`

---
+++ rollup.config.js
@@ -0,0 +1,89 @@
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+import { dts } from 'rollup-plugin-dts'
+import filesize from 'rollup-plugin-filesize'
+import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser'
+import { resolveExtensions } from './build/rollup-plugin-resolve-extensions/index.js'
+
+const external = [
+  '@noble/secp256k1',
+  '@xmtp/proto',
+  '@xmtp/user-preferences-bindings-wasm',
+  '@xmtp/user-preferences-bindings-wasm/web',
+  '@xmtp/user-preferences-bindings-wasm/bundler',
+  'async-mutex',
+  'crypto',
+  'elliptic',
+  'ethers',
+  'js-sha3',
+  'long',
+]
+
+const plugins = [
+  typescript({
+    declaration: false,
+    declarationMap: false,
+  }),
+  filesize({
+    showMinifiedSize: false,
+  }),
+  json({
+    preferConst: true,
+  }),
+]
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins,
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins,
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/browser/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [resolveExtensions(['.browser']), terser(), ...plugins],
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/bundler/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [
+      resolveExtensions(['.bundler', '.browser']),
+      terser(),
+      ...plugins,
+    ],
+    external,
+  },
+])
