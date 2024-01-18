b338919dcdd1621c215e01b0b3484a2fc4685b4b
Ry Racherbaumer <ry@xmtp.com>
2024-01-17 21:51:26 -0600

    test: update `TestKeyCodec`

--- test/ContentTypeTestKey.ts
+++ test/ContentTypeTestKey.ts
@@ -30,4 +30,8 @@ export class TestKeyCodec implements ContentCodec<PublicKey> {
   fallback(content: PublicKey): string | undefined {
     return 'publickey bundle'
   }
+
+  shouldPush() {
+    return false
+  }
 }
