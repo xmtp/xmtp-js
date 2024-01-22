c02864db3ff4176719a50772569eb3556251e874
Ry Racherbaumer <ry@xmtp.com>
2024-01-22 17:05:57 -0600

    Replace `tsup` with `rollup`

--- packages/content-type-remote-attachment/src/encryption.ts
+++ packages/content-type-remote-attachment/src/encryption.ts
@@ -1,12 +1,3 @@
-/**
- * This file is necessary to ensure that the crypto library is available
- * in node and the browser
- */
-import { webcrypto as nodeCrypto } from "crypto";
+import { webcrypto } from "node:crypto";
 
-const webcrypto =
-  typeof globalThis === "object" && "crypto" in globalThis
-    ? (globalThis.crypto as nodeCrypto.Crypto)
-    : undefined;
-
-export const crypto = webcrypto ?? nodeCrypto;
+export const crypto = webcrypto;
