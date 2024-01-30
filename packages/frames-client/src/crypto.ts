171e0175fe8e67d24f913a48c0d9442238a48c17
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-01-30 09:15:50 -0800

    Dynamic import of crypto

--- packages/frames-client/src/crypto.ts
+++ packages/frames-client/src/crypto.ts
@@ -1,9 +1,16 @@
-// eslint-disable-next-line
-const webCrypto: Crypto =
-  // eslint-disable-next-line
-  typeof crypto === "undefined" ? require("crypto").webcrypto : crypto;
+async function getCrypto(): Promise<Crypto> {
+  if (typeof crypto !== "undefined") {
+    return crypto;
+  }
+
+  if (typeof window !== "undefined" && window.crypto) {
+    return window.crypto;
+  }
+
+  return (await import("crypto")).webcrypto as Crypto;
+}
 
 export async function sha256(data: Uint8Array): Promise<Uint8Array> {
-  const digest = await webCrypto.subtle.digest("SHA-256", data);
+  const digest = await (await getCrypto()).subtle.digest("SHA-256", data);
   return new Uint8Array(digest);
 }
