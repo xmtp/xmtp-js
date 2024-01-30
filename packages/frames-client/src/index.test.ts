a40553a2e4ad83c84c27da2450cf7fc999cc23e6
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-01-29 18:50:56 -0800

    Add some more tests

--- packages/frames-client/src/index.test.ts
+++ packages/frames-client/src/index.test.ts
@@ -1,11 +1,61 @@
-import { Client, PrivateKey } from "@xmtp/xmtp-js";
+import { Client } from "@xmtp/xmtp-js";
 import { Wallet } from "ethers";
+import { frames, fetcher } from "@xmtp/proto";
 import { it, expect, describe, beforeEach } from "vitest";
+import { FramesClient } from ".";
+
+const { b64Decode } = fetcher;
 
 describe("signFrameAction", () => {
   let client: Client;
+  let framesClient: FramesClient;
   beforeEach(async () => {
     client = await Client.create(Wallet.createRandom());
+    framesClient = new FramesClient(client);
   });
-  it("should sign a frame action with a valid signature", async () => {});
+  it("should sign a frame action with a valid signature", async () => {
+    const frameUrl = "https://example.com";
+    const buttonIndex = 1;
+    const conversationIdentifier = "testConversationIdentifier";
+    const messageId = "testMessageId";
+
+    const signedPayload = await framesClient.signFrameAction(
+      frameUrl,
+      buttonIndex,
+      conversationIdentifier,
+      messageId,
+    );
+
+    expect(signedPayload.untrustedData.walletAddress).toEqual(client.address);
+    expect(signedPayload.untrustedData.url).toEqual(frameUrl);
+    expect(signedPayload.untrustedData.buttonIndex).toEqual(buttonIndex);
+    expect(signedPayload.untrustedData.conversationIdentifier).toEqual(
+      conversationIdentifier,
+    );
+    expect(signedPayload.untrustedData.timestamp).toBeGreaterThan(0);
+
+    const signedPayloadProto = frames.FrameAction.decode(
+      b64Decode(signedPayload.trustedData.messageBytes),
+    );
+    expect(signedPayloadProto.actionBody).toBeDefined();
+    expect(signedPayloadProto.signature).toBeDefined();
+    expect(signedPayloadProto.signedPublicKeyBundle).toBeDefined();
+
+    const signedPayloadBody = frames.FrameActionBody.decode(
+      signedPayloadProto.actionBody,
+    );
+    expect(signedPayloadBody.messageId).toEqual(messageId);
+    expect(new TextDecoder().decode(signedPayloadBody.buttonIndex)).toEqual(
+      buttonIndex.toString(),
+    );
+    expect(new TextDecoder().decode(signedPayloadBody.frameUrl)).toEqual(
+      frameUrl,
+    );
+    expect(signedPayloadBody.conversationIdentifier).toEqual(
+      conversationIdentifier,
+    );
+    expect(new TextDecoder().decode(signedPayloadBody.frameUrl)).toEqual(
+      frameUrl,
+    );
+  });
 });
