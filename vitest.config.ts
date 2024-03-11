e621ceef4136075e30bc32ff7f4c14e89d38b265
Ry Racherbaumer <ry@xmtp.com>
2024-03-11 18:34:41 -0500

    test: double test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    testTimeout: 60000,
+    testTimeout: 120000,
     hookTimeout: 60000,
   },
 })
