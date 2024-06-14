96bdeb214712acdace6e92c7904f166a2653303f
Ry Racherbaumer <ry@xmtp.com>
2024-06-14 10:15:06 -0500

    Add production environment

--- packages/mls-client/src/Client.ts
+++ packages/mls-client/src/Client.ts
@@ -23,6 +23,7 @@ import { Conversations } from '@/Conversations'
 export const ApiUrls = {
   local: 'http://localhost:5556',
   dev: 'https://grpc.dev.xmtp.network:443',
+  production: 'https://grpc.production.xmtp.network:443',
 } as const
 
 export type XmtpEnv = keyof typeof ApiUrls
