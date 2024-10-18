2079f59adc3d36a72c4cb2cf0aac9fb7639f7a04
Ry Racherbaumer <ry@xmtp.com>
2024-10-18 10:33:44 -0500

    Add registration when creating a client

--- examples/react-vite-browser-sdk/src/createClient.ts
+++ examples/react-vite-browser-sdk/src/createClient.ts
@@ -1,10 +1,32 @@
 import { Client } from "@xmtp/browser-sdk";
+import { toBytes } from "viem/utils";
 import { createWallet } from "./wallets";
+
+type Wallet = ReturnType<typeof createWallet>;
+
+export const getSignature = async (client: Client, wallet: Wallet) => {
+  const signatureText = await client.getSignatureText();
+  if (signatureText) {
+    const signature = await wallet.signMessage({
+      message: signatureText,
+    });
+    return toBytes(signature);
+  }
+  return null;
+};
 
 export const createClient = async (walletKey: string) => {
   const wallet = createWallet(walletKey);
   const client = await Client.create(wallet.account.address, {
     env: "local",
   });
+  const isRegistered = await client.isRegistered();
+  if (!isRegistered) {
+    const signature = await getSignature(client, wallet);
+    if (signature) {
+      await client.addSignature(signature);
+    }
+    await client.registerIdentity();
+  }
   return client;
 };
