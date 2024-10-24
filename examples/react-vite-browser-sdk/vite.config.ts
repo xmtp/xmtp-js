54632f16e0f76cf92da727f1f7fb9027332e4617
Ry Racherbaumer <ry@xmtp.com>
2024-10-24 14:58:29 -0500

    Add Browser SDK example

---
+++ examples/react-vite-browser-sdk/vite.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@xmtp/wasm-bindings"],
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+});
