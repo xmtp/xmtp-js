2516903c980e082e5e665b73bef84b73037ce992
Ry Racherbaumer <ry@xmtp.com>
2024-11-13 10:49:15 -0600

    Update node bindings exports

--- sdks/node-sdk/src/index.ts
+++ sdks/node-sdk/src/index.ts
@@ -10,5 +10,33 @@ export { Conversation } from "./Conversation";
 export { Conversations } from "./Conversations";
 export { DecodedMessage } from "./DecodedMessage";
 export type { StreamCallback } from "./AsyncStream";
-export type * from "@xmtp/node-bindings";
+export type {
+  Consent,
+  ContentTypeId,
+  CreateGroupOptions,
+  EncodedContent,
+  InboxState,
+  Installation,
+  ListConversationsOptions,
+  ListMessagesOptions,
+  Message,
+  PermissionPolicySet,
+} from "@xmtp/node-bindings";
+export {
+  ConsentEntityType,
+  ConsentState,
+  ConversationType,
+  DeliveryStatus,
+  GroupMember,
+  GroupMembershipState,
+  GroupMessageKind,
+  GroupMetadata,
+  GroupPermissions,
+  GroupPermissionsOptions,
+  PermissionLevel,
+  PermissionPolicy,
+  PermissionUpdateType,
+  SignatureRequestType,
+  SortDirection,
+} from "@xmtp/node-bindings";
 export { generateInboxId, getInboxIdForAddress } from "./helpers/inboxId";
