e1a6a7b87d1800fb61264857eda57aa7ae91f4a7
Ry Racherbaumer <ry@xmtp.com>
2024-11-14 14:45:20 -0600

    Remove all DBs after testing

--- sdks/node-sdk/vitest.setup.ts
+++ sdks/node-sdk/vitest.setup.ts
@@ -4,9 +4,8 @@ import { fileURLToPath } from "node:url";
 import { glob } from "fast-glob";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const testPath = join(__dirname, "test");
 
 export const teardown = async () => {
-  const files = await glob("test-*.db3*", { cwd: testPath });
-  await Promise.all(files.map((file) => unlink(join(testPath, file))));
+  const files = await glob("**/*.db3*", { cwd: __dirname });
+  await Promise.all(files.map((file) => unlink(join(__dirname, file))));
 };
