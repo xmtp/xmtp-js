c8e15684a65761f8c35d4b853f83d47bb0ebcbb3
Naomi Plasterer <naomi@xmtp.com>
2024-11-27 18:35:10 -0800

    update the frame url

--- packages/frames-client/src/client.test.ts
+++ packages/frames-client/src/client.test.ts
@@ -133,7 +133,7 @@ const shouldSignFrameActionWithValidSignature =
 // Will add E2E tests back once we have Frames deployed with the new schema
 const worksE2E = (framesClient: FramesClient) => async () => {
   const frameUrl =
-    "https://fc-polls-five.vercel.app/polls/01032f47-e976-42ee-9e3d-3aac1324f4b8";
+    "https://fc-polls-aikbbaraw-ephemerahq.vercel.app/polls/95de04bf-3f35-4042-895a-094e50062b1e;
   const metadata = await framesClient.proxy.readMetadata(frameUrl);
   expect(metadata).toBeDefined();
   expect(metadata.frameInfo).toMatchObject({
@@ -150,10 +150,10 @@ const worksE2E = (framesClient: FramesClient) => async () => {
     },
     image: {
       content:
-        "https://fc-polls-five.vercel.app/api/image?id=01032f47-e976-42ee-9e3d-3aac1324f4b8",
+        "https://fc-polls-aikbbaraw-ephemerahq.vercel.app/api/image?id=01032f47-e976-42ee-9e3d-3aac1324f4b8",
     },
     postUrl:
-      "https://fc-polls-five.vercel.app/api/vote?id=01032f47-e976-42ee-9e3d-3aac1324f4b8",
+      "https://fc-polls-aikbbaraw-ephemerahq.vercel.app/api/vote?id=01032f47-e976-42ee-9e3d-3aac1324f4b8",
   });
   const signedPayload = await framesClient.signFrameAction({
     frameUrl,
