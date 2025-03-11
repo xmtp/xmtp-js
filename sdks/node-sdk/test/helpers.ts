8c22d0f02048bd9ef7c361e1df4356f126ffe65b
Ry Racherbaumer <ry@xmtp.com>
2025-03-11 12:52:42 -0500

    Add PasskeyAuthenticator for testing

--- sdks/node-sdk/test/helpers.ts
+++ sdks/node-sdk/test/helpers.ts
@@ -1,4 +1,10 @@
-import { getRandomValues } from "node:crypto";
+import {
+  createHash,
+  createSign,
+  generateKeyPairSync,
+  getRandomValues,
+  type KeyPairKeyObjectResult,
+} from "node:crypto";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import {
@@ -6,7 +12,16 @@ import {
   type ContentCodec,
   type EncodedContent,
 } from "@xmtp/content-type-primitives";
-import { generateInboxId, IdentifierKind } from "@xmtp/node-bindings";
+import {
+  generateInboxId,
+  IdentifierKind,
+  type Identifier,
+} from "@xmtp/node-bindings";
+import {
+  hexToUint8Array,
+  uint8ArrayToBase64,
+  uint8ArrayToHex,
+} from "uint8array-extras";
 import { createWalletClient, http, toBytes } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
@@ -113,5 +128,87 @@ export class TestCodec implements ContentCodec<Record<string, string>> {
 
   shouldPush() {
     return false;
+  }
+}
+
+export class PasskeyAuthenticator {
+  #keyPair: KeyPairKeyObjectResult;
+
+  constructor() {
+    this.#keyPair = generateKeyPairSync("ec", {
+      namedCurve: "prime256v1",
+    });
+  }
+
+  get publicKeyString() {
+    return uint8ArrayToHex(this.publicKey);
+  }
+
+  get publicKey() {
+    const publicKey = this.#keyPair.publicKey
+      .export({
+        type: "spki",
+        format: "der",
+      })
+      .toString("hex");
+    const publicKeyBytes = hexToUint8Array(publicKey);
+    if (publicKeyBytes.length < 27) {
+      throw new Error("Invalid public key");
+    }
+    return publicKeyBytes.slice(26);
+  }
+
+  get identifier(): Identifier {
+    return {
+      identifier: this.publicKeyString,
+      identifierKind: IdentifierKind.Passkey,
+    };
+  }
+
+  #generateClientDataJson(message: string) {
+    const messageBytes = new TextEncoder().encode(message);
+    const json = {
+      type: "webauthn.get",
+      origin: "https://xmtp.chat",
+      crossOrigin: false,
+      challenge: uint8ArrayToBase64(messageBytes),
+    };
+    return new TextEncoder().encode(JSON.stringify(json));
+  }
+
+  #generateAuthenticatorData() {
+    const messageBytes = new TextEncoder().encode("xmtp.chat");
+    const hash = createHash("sha256").update(messageBytes).digest("hex");
+    const hashBytes = hexToUint8Array(hash);
+    const flags = new Uint8Array([1]);
+    const signCount = new Uint8Array([0, 0, 0, 1]);
+    return new Uint8Array([...hashBytes, ...flags, ...signCount]);
+  }
+
+  #signPasskeyData(clientData: Uint8Array, authenticatorData: Uint8Array) {
+    const clientDataHash = createHash("sha256")
+      .update(clientData)
+      .digest("hex");
+    const clientDataHashBytes = hexToUint8Array(clientDataHash);
+    const signedData = new Uint8Array([
+      ...authenticatorData,
+      ...clientDataHashBytes,
+    ]);
+    const sign = createSign("SHA256");
+    sign.update(signedData);
+    sign.end();
+    const signature = sign.sign(this.#keyPair.privateKey, "hex");
+    return hexToUint8Array(signature);
+  }
+
+  sign(message: string) {
+    const clientDataJson = this.#generateClientDataJson(message);
+    const authenticatorData = this.#generateAuthenticatorData();
+    const signature = this.#signPasskeyData(clientDataJson, authenticatorData);
+    return {
+      clientDataJson,
+      authenticatorData,
+      signature,
+    };
   }
 }
