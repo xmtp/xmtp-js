0f40665f5b24ab5ccb70b472083bdba650ccdeee
Ry Racherbaumer <ry@xmtp.com>
2025-03-11 10:57:58 -0500

    Update signer

--- sdks/node-sdk/src/helpers/signer.ts
+++ sdks/node-sdk/src/helpers/signer.ts
@@ -1,18 +1,23 @@
+import type { Identifier } from "@xmtp/node-bindings";
+
 export type SignMessage = (message: string) => Promise<Uint8Array> | Uint8Array;
-export type GetAddress = () => Promise<string> | string;
+export type GetIdentifier = () => Promise<Identifier> | Identifier;
 export type GetChainId = () => bigint;
 export type GetBlockNumber = () => bigint;
 
 export type Signer =
   | {
-      walletType: "EOA";
-      getAddress: GetAddress;
+      type: "EOA";
       signMessage: SignMessage;
+      getIdentifier: GetIdentifier;
     }
   | {
-      walletType: "SCW";
-      getAddress: GetAddress;
+      type: "SCW";
       signMessage: SignMessage;
+      getIdentifier: GetIdentifier;
       getBlockNumber?: GetBlockNumber;
       getChainId: GetChainId;
     };
+
+export type EOASigner = Extract<Signer, { type: "EOA" }>;
+export type SCWSigner = Extract<Signer, { type: "SCW" }>;
