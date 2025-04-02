230be462cde34aad285871a60cb3bc8c669bce5d
Ry Racherbaumer <ry@xmtp.com>
2025-04-01 22:52:00 -0500

    Update select conversation page

--- apps/xmtp.chat/src/components/App/SelectConversation.tsx
+++ apps/xmtp.chat/src/components/App/SelectConversation.tsx
@@ -1,26 +1,24 @@
 import { Button, Stack, Text, Title } from "@mantine/core";
-import { useRefManager } from "@/contexts/RefManager";
+import { useNavigate } from "react-router";
 
 export const SelectConversation = () => {
-  const { getRef } = useRefManager();
-
+  const navigate = useNavigate();
   return (
     <Stack gap={60} p="md" align="center">
-      <Stack gap="lg">
+      <Stack gap="lg" align="center">
         <Title order={3}>No conversation selected</Title>
         <Text>
-          Select a conversation in the left sidebar to display its messages, or
-          create a new conversation by clicking the{" "}
-          <Button
-            size="xs"
-            px={6}
-            onClick={() => {
-              getRef("new-conversation-button")?.current?.click();
-            }}>
-            New conversation
-          </Button>{" "}
-          button.
+          Select a conversation in the left sidebar to display its messages,
+          or...
         </Text>
+        <Button
+          size="xs"
+          px={6}
+          onClick={() => {
+            void navigate("/conversations/new");
+          }}>
+          Create a new conversation
+        </Button>
       </Stack>
     </Stack>
   );
