7ec28c8391370836d51f3b4ebd729486f6df5e88
J Kishore Kumar <kishore@heyelsa.ai>
2025-04-12 00:51:59 +0530

    Batched transaction support for smart wallets
    Added hooks for capabilities and call status, refactored handleSubmit to utilize sendCalls for atomic batching when supported(Eg: Coinbase Smart Wallet), and updated transaction hash management.

--- apps/xmtp.chat/src/components/Messages/WalletSendCallsContent.tsx
+++ apps/xmtp.chat/src/components/Messages/WalletSendCallsContent.tsx
@@ -5,9 +5,19 @@ import {
   type TransactionReference,
 } from "@xmtp/content-type-transaction-reference";
 import type { WalletSendCallsParams } from "@xmtp/content-type-wallet-send-calls";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useOutletContext } from "react-router";
-import { useChainId, useSendTransaction, useSwitchChain } from "wagmi";
+import {
+  useAccount,
+  useChainId,
+  useSendTransaction,
+  useSwitchChain,
+} from "wagmi";
+import {
+  useCapabilities,
+  useSendCalls,
+  useWaitForCallsStatus,
+} from "wagmi/experimental";
 
 export type WalletSendCallsContentProps = {
   content: WalletSendCallsParams;
@@ -20,30 +30,40 @@ export const WalletSendCallsContent: React.FC<WalletSendCallsContentProps> = ({
 }) => {
   const { client } = useOutletContext<{ client: Client }>();
   const { sendTransactionAsync } = useSendTransaction();
+  const wagmiAccount = useAccount();
+  const { sendCalls } = useSendCalls();
+  const [callsId, setCallsId] = useState<string>();
+  const { data: callsStatus } = useWaitForCallsStatus({
+    id: callsId,
+    query: {
+      enabled: !!callsId,
+    },
+  });
+  const [txHash, setTxHash] = useState<`0x${string}`>();
   const { switchChainAsync } = useSwitchChain();
   const wagmiChainId = useChainId();
+  type OptionalCapability = { supported: boolean } | undefined;
+  const { data: availableCapabilities } = useCapabilities();
 
-  const handleSubmit = useCallback(async () => {
-    const chainId = parseInt(content.chainId, 16);
-    if (chainId !== wagmiChainId) {
-      console.log(
-        `Current Chain Id (${wagmiChainId}) doesn't match; switching to Chain Id ${chainId}.`,
-      );
-      await switchChainAsync({ chainId });
-      await new Promise((r) => setTimeout(r, 300)); // Metamask requires some delay
-    }
-    for (const call of content.calls) {
-      const wagmiTxData = {
-        ...call,
-        value: BigInt(parseInt(call.value || "0x0", 16)),
-        chainId,
-        gas: call.gas ? BigInt(parseInt(call.gas, 16)) : undefined,
-      };
-      const txHash = await sendTransactionAsync(wagmiTxData, {
-        onError(error) {
-          console.error(error);
-        },
-      });
+  const isBatchingSupported = (txChainId: number): boolean => {
+    return !!(
+      availableCapabilities &&
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- Necessary because Viem type definition is bad and availableCapabilities is {} in case of an EOA wallet.
+      (availableCapabilities[txChainId]?.atomicBatch as OptionalCapability)
+    )?.supported;
+  };
+
+  useEffect(() => {
+    if (!callsStatus?.receipts) return;
+    // Exactly one receipt is expected since sendCalls is used only when atomic batching is supported
+    const receipt = callsStatus.receipts[0];
+    console.log("setting tx hash");
+    setTxHash(receipt.transactionHash);
+  }, [callsStatus?.status]);
+
+  useEffect(() => {
+    if (!txHash) return;
+    void (async () => {
       const transactionReference: TransactionReference = {
         networkId: content.chainId,
         reference: txHash,
@@ -58,8 +78,58 @@ export const WalletSendCallsContent: React.FC<WalletSendCallsContentProps> = ({
         transactionReference,
         ContentTypeTransactionReference,
       );
+    })();
+  }, [txHash]);
+
+  const handleSubmit = useCallback(async () => {
+    const chainId = parseInt(content.chainId, 16);
+    if (chainId !== wagmiChainId) {
+      console.log(
+        `Current Chain Id (${wagmiChainId}) doesn't match; switching to Chain Id ${chainId}.`,
+      );
+      await switchChainAsync({ chainId });
+      await new Promise((r) => setTimeout(r, 300)); // Metamask requires some delay
     }
-  }, [content, sendTransactionAsync, client, conversationId]);
+    if (isBatchingSupported(chainId)) {
+      sendCalls(
+        {
+          calls: content.calls,
+          chainId,
+        },
+        {
+          onSuccess({ id }) {
+            console.log("Send Calls Id:", id);
+            setCallsId(id);
+          },
+          onError(error) {
+            console.error(error);
+          },
+        },
+      );
+    } else {
+      for (const call of content.calls) {
+        const wagmiTxData = {
+          ...call,
+          value: BigInt(parseInt(call.value || "0x0", 16)),
+          chainId,
+          gas: call.gas ? BigInt(parseInt(call.gas, 16)) : undefined,
+        };
+        const txHash = await sendTransactionAsync(wagmiTxData, {
+          onError(error) {
+            console.error(error);
+          },
+        });
+        setTxHash(txHash);
+      }
+    }
+  }, [
+    content,
+    sendTransactionAsync,
+    client,
+    conversationId,
+    wagmiAccount.isConnected,
+    wagmiAccount.connector,
+  ]);
 
   return (
     <Box flex="flex">
