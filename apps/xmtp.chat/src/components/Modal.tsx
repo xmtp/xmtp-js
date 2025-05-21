901a61e1d09ebfac3740d7d2e75bf7cd9f489f5c
Ry Racherbaumer <rygine@users.noreply.github.com>
2025-05-21 11:21:05 -0500

    Update xmtp.chat blockchains (#1029)

--- apps/xmtp.chat/src/components/Modal.tsx
+++ apps/xmtp.chat/src/components/Modal.tsx
@@ -4,6 +4,7 @@ export const Modal: React.FC<ModalProps> = ({ children, ...props }) => {
   return (
     <MantineModal
       {...props}
+      radius="md"
       styles={{
         content: {
           display: "flex",
