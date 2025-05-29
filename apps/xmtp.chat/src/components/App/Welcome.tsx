044be31c56545d5bad7f696e76e130507fab6b5d
Ry Racherbaumer <ry@xmtp.com>
2025-05-29 13:04:32 -0500

    Fix wallet auto-connect on xmtp.chat

--- apps/xmtp.chat/src/components/App/Welcome.tsx
+++ apps/xmtp.chat/src/components/App/Welcome.tsx
@@ -6,19 +6,104 @@ import {
   Title,
   useMatches,
 } from "@mantine/core";
-import { Outlet } from "react-router";
-import { useConnect } from "wagmi";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { hexToUint8Array } from "uint8array-extras";
+import { generatePrivateKey } from "viem/accounts";
+import { useAccount, useConnect, useSignMessage } from "wagmi";
 import { Connect } from "@/components/App/Connect";
 import { Settings } from "@/components/App/Settings";
 import { useXMTP } from "@/contexts/XMTPContext";
+import {
+  createEOASigner,
+  createEphemeralSigner,
+  createSCWSigner,
+} from "@/helpers/createSigner";
+import { useRedirect } from "@/hooks/useRedirect";
+import { useSettings } from "@/hooks/useSettings";
 
 export const Welcome = () => {
   const { status } = useConnect();
-  const { initializing } = useXMTP();
+  const { initializing, client, initialize } = useXMTP();
+  const navigate = useNavigate();
+  const account = useAccount();
+  const { redirectUrl, setRedirectUrl } = useRedirect();
+  const { signMessageAsync } = useSignMessage();
+  const {
+    encryptionKey,
+    environment,
+    ephemeralAccountEnabled,
+    ephemeralAccountKey,
+    setEphemeralAccountKey,
+    loggingLevel,
+    useSCW,
+  } = useSettings();
   const px = useMatches({
     base: "5%",
     sm: "10%",
   });
+
+  // redirect if there's already a client
+  useEffect(() => {
+    if (client) {
+      if (redirectUrl) {
+        setRedirectUrl("");
+        void navigate(redirectUrl);
+      } else {
+        void navigate("/");
+      }
+    }
+  }, [client]);
+
+  // create client if ephemeral account is enabled
+  useEffect(() => {
+    if (ephemeralAccountEnabled) {
+      let accountKey = ephemeralAccountKey;
+      if (!accountKey) {
+        accountKey = generatePrivateKey();
+        setEphemeralAccountKey(accountKey);
+      }
+
+      const signer = createEphemeralSigner(accountKey);
+      void initialize({
+        dbEncryptionKey: encryptionKey
+          ? hexToUint8Array(encryptionKey)
+          : undefined,
+        env: environment,
+        loggingLevel,
+        signer,
+      });
+    }
+  }, [
+    ephemeralAccountEnabled,
+    ephemeralAccountKey,
+    encryptionKey,
+    environment,
+    loggingLevel,
+  ]);
+
+  // create client if wallet is connected
+  useEffect(() => {
+    if (!account.address || (useSCW && !account.chainId)) {
+      return;
+    }
+    void initialize({
+      dbEncryptionKey: encryptionKey
+        ? hexToUint8Array(encryptionKey)
+        : undefined,
+      env: environment,
+      loggingLevel,
+      signer: useSCW
+        ? createSCWSigner(
+            account.address,
+            (message: string) => signMessageAsync({ message }),
+            account.chainId,
+          )
+        : createEOASigner(account.address, (message: string) =>
+            signMessageAsync({ message }),
+          ),
+    });
+  }, [account.address, account.chainId, useSCW, signMessageAsync]);
 
   const isBusy = status === "pending" || initializing;
 
