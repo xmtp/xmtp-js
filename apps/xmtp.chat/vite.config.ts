77e8a9d2eeca13dd9e7332a519b75fe48eced07d
Ry Racherbaumer <ry@xmtp.com>
2025-07-11 15:46:19 -0500

    Update vite config

--- apps/xmtp.chat/vite.config.ts
+++ apps/xmtp.chat/vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   optimizeDeps: {
-    exclude: ["@xmtp/wasm-bindings"],
+    exclude: ["@xmtp/browser-sdk"],
   },
 });
