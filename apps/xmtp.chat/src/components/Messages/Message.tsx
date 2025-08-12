4b6fbf5f260dd7a96576bd027ab1fd5bbddfdae5
Benny Neugebauer <mail@bennycode.com>
2025-08-13 00:43:40 +0200

    drill onReply prop

--- apps/xmtp.chat/src/components/Messages/Message.tsx
+++ apps/xmtp.chat/src/components/Messages/Message.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mantine/core";
+import { Box, Button, Group } from "@mantine/core";
 import type { Client, DecodedMessage } from "@xmtp/browser-sdk";
+import { ContentTypeText } from "@xmtp/content-type-text";
 import { useNavigate, useOutletContext } from "react-router";
 import classes from "./Message.module.css";
 import { MessageContentWithWrapper } from "./MessageContentWithWrapper";
@@ -7,39 +8,52 @@ import { MessageContentWithWrapper } from "./MessageContentWithWrapper";
 export type MessageProps = {
   message: DecodedMessage;
   scrollToMessage: (id: string) => void;
+  onReply?: (message: DecodedMessage) => void;
 };
 
 export const Message: React.FC<MessageProps> = ({
   message,
   scrollToMessage,
+  onReply,
 }) => {
   const { client } = useOutletContext<{ client: Client }>();
   const isSender = client.inboxId === message.senderInboxId;
   const align = isSender ? "right" : "left";
   const navigate = useNavigate();
+  const isText = message.contentType.sameAs(ContentTypeText);
+  const showReply = !!onReply && isText;
   return (
-    <Box
-      p="md"
-      tabIndex={0}
-      className={classes.root}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
+    <Box p="md" tabIndex={0} className={classes.root}>
+      <Box
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            void navigate(
+              `/conversations/${message.conversationId}/message/${message.id}`,
+            );
+          }
+        }}
+        onClick={() =>
           void navigate(
             `/conversations/${message.conversationId}/message/${message.id}`,
-          );
-        }
-      }}
-      onClick={() =>
-        void navigate(
-          `/conversations/${message.conversationId}/message/${message.id}`,
-        )
-      }>
-      <MessageContentWithWrapper
-        message={message}
-        align={align}
-        senderInboxId={message.senderInboxId}
-        scrollToMessage={scrollToMessage}
-      />
+          )
+        }>
+        <MessageContentWithWrapper
+          message={message}
+          align={align}
+          senderInboxId={message.senderInboxId}
+          scrollToMessage={scrollToMessage}
+        />
+      </Box>
+      {showReply && (
+        <Group justify={align === "left" ? "flex-start" : "flex-end"} mt={4}>
+          <Button
+            size="compact-xs"
+            variant="subtle"
+            onClick={() => onReply?.(message)}>
+            Reply
+          </Button>
+        </Group>
+      )}
     </Box>
   );
 };
