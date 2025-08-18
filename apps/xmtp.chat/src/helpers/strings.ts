7ceb9bbf2f8d13e53437a36d0c6fc6887d8907c4
Benny Neugebauer <mail@bennycode.com>
2025-08-18 22:20:53 +0200

    use message.fallback

--- apps/xmtp.chat/src/helpers/strings.ts
+++ apps/xmtp.chat/src/helpers/strings.ts
@@ -11,15 +11,11 @@ export const shortAddress = (address: string): string =>
   `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
 
 export const stringifyMessage = (message: DecodedMessage) => {
-  if ("content" in message) {
-    if (
-      message.content &&
-      typeof message.content === "object" &&
-      "content" in message.content
-    ) {
+  if (message["content"]) {
+    if (typeof message.content === "object" && "content" in message.content) {
       return String(message.content.content);
     }
     return String(message.content);
   }
-  return String(message);
+  return message.fallback ? message.fallback : String(message);
 };
