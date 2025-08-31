59d27d81b04701a7b93bbb2aef35ac13ce961ed2
Benny Neugebauer <mail@bennycode.com>
2025-09-01 01:47:34 +0200

    re-exports

--- sdks/agent-sdk/src/index.ts
+++ sdks/agent-sdk/src/index.ts
@@ -1,3 +1,16 @@
 export * from "./core/index.js";
 export * from "./middleware/index.js";
 export * from "./utils/index.js";
+// Re-export imports from the Node SDK
+export {
+  ApiUrls,
+  Client,
+  ClientOptions,
+  Conversation,
+  DecodedMessage,
+  Identifier,
+  IdentifierKind,
+  LogLevel,
+  Signer,
+  XmtpEnv,
+} from "@xmtp/node-sdk";
