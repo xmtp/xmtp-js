3c4988588c16903760af40d413dda34512ce9a1b
Benny Neugebauer <mail@bennycode.com>
2025-09-16 00:32:35 +0200

    revert

--- sdks/node-sdk/test/Conversations.test.ts
+++ sdks/node-sdk/test/Conversations.test.ts
@@ -433,7 +433,7 @@ describe("Conversations", () => {
     }
 
     expect(receivedIds.length).toBe(2);
-    expect(receivedIds.sort()).toMatchObject(expectedIds.sort());
+    expect(receivedIds.sort()).toEqual(expectedIds.sort());
     expect(
       (await client3.conversations.getConversationById(conversation1.id))?.id,
     ).toBe(conversation1.id);
