e2701a1e5270c1adedcd469f6ba06040dbb78f1f
Ry Racherbaumer <ry@xmtp.com>
2025-09-17 17:44:11 -0500

    Add web3.bio env var

--- apps/xmtp.chat-api-service/src/globals.d.ts
+++ apps/xmtp.chat-api-service/src/globals.d.ts
@@ -4,6 +4,7 @@ declare global {
       readonly PINATA_JWT: string;
       readonly PINATA_GATEWAY: string;
       readonly PINATA_GROUP_ID: string;
+      readonly WEB3_BIO_API_KEY: string;
     }
   }
 }
