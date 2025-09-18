06af2f0053e5218cd0b4315a0f63a47bedd37aba
Ry Racherbaumer <ry@xmtp.com>
2025-09-18 15:20:00 -0500

    Adjust env var

--- apps/xmtp.chat-api-service/src/globals.d.ts
+++ apps/xmtp.chat-api-service/src/globals.d.ts
@@ -4,7 +4,7 @@ declare global {
       readonly PINATA_JWT: string;
       readonly PINATA_GATEWAY: string;
       readonly PINATA_GROUP_ID: string;
-      readonly WEB3_BIO_API_KEY: string;
+      readonly WEB3BIO_API_KEY: string;
     }
   }
 }
