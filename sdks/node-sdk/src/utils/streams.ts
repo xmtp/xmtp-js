83ae136bbd15befc9e998c640ca9320bc29462a5
Benny Neugebauer <mail@bennycode.com>
2025-09-19 23:51:20 +0200

    macroscopeapp

--- sdks/node-sdk/src/utils/streams.ts
+++ sdks/node-sdk/src/utils/streams.ts
@@ -140,6 +140,7 @@ export const createStream = async <T = unknown, V = T>(
     if (retries === 0) {
       void asyncStream.end();
       onError?.(new StreamFailedError(retryAttempts));
+      return;
     }
 
     // wait for the retry delay before attempting to restart the stream
