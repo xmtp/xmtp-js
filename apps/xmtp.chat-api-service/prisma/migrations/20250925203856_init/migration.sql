89080361cfe862bc7f5ab2afe7edf0b5b634a98a
Ry Racherbaumer <rygine@users.noreply.github.com>
2025-09-25 16:39:13 -0500

    Refactor API schema and endpoints (#1244)
    * Update schema
    
    * Refactor endpoints
    
    * Update rollup config
    
    * Update build command
    
    * Use the filtered input

--- apps/xmtp.chat-api-service/prisma/migrations/20250925203856_init/migration.sql
+++ apps/xmtp.chat-api-service/prisma/migrations/20250925203856_init/migration.sql
@@ -1,18 +1,15 @@
 -- CreateEnum
-CREATE TYPE "public"."Platform" AS ENUM ('ens', 'basenames', 'farcaster', 'unknown');
+CREATE TYPE "public"."Platform" AS ENUM ('ens', 'basenames', 'unknown');
 
 -- CreateTable
 CREATE TABLE "public"."Profile" (
     "id" TEXT NOT NULL,
     "address" TEXT NOT NULL,
-    "identity" TEXT NOT NULL,
-    "platform" "public"."Platform" NOT NULL,
-    "displayName" TEXT,
-    "email" TEXT,
-    "location" TEXT,
-    "status" TEXT,
     "avatar" TEXT,
     "description" TEXT,
+    "displayName" TEXT,
+    "identity" TEXT NOT NULL,
+    "platform" "public"."Platform" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
