8f76f2dd167394af2b8c4de9c86664898352d54f
Ry Racherbaumer <ry@xmtp.com>
2025-10-08 10:57:28 -0500

    Add date helper

--- apps/xmtp.chat/src/helpers/date.ts
+++ apps/xmtp.chat/src/helpers/date.ts
@@ -1,3 +1,7 @@
-export function nsToDate(ns: bigint): Date {
+export const nsToDate = (ns: bigint): Date => {
   return new Date(Number(ns / 1_000_000n));
-}
+};
+
+export const dateToNs = (date: Date): bigint => {
+  return BigInt(date.getTime() * 1_000_000);
+};
