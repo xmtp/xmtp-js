cceffbde07d42a75efc065a437f3a4ad36813aaf
Ry Racherbaumer <ry@xmtp.com>
2025-10-08 15:26:41 -0500

    Add props to IconDots

--- apps/xmtp.chat/src/icons/IconDots.tsx
+++ apps/xmtp.chat/src/icons/IconDots.tsx
@@ -1,15 +1,23 @@
-export const IconDots = () => {
+export const IconDots = ({
+  size = 20,
+  color = "currentColor",
+  ...props
+}: {
+  size?: number;
+  color?: string;
+}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       strokeWidth="2"
       strokeLinecap="round"
-      strokeLinejoin="round">
+      strokeLinejoin="round"
+      {...props}>
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
       <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
