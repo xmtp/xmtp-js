eb9af4d02f2ee9f23c39f141cb2f1807c0ed3926
Ry Racherbaumer <ry@xmtp.com>
2025-10-18 15:31:23 -0500

    Use cache when resolving names

--- apps/xmtp.chat/src/helpers/names.ts
+++ apps/xmtp.chat/src/helpers/names.ts
@@ -1,6 +1,7 @@
 import { Utils, type XmtpEnv } from "@xmtp/browser-sdk";
 import { queryClient } from "@/helpers/queries";
 import { isValidEthereumAddress } from "@/helpers/strings";
+import { profilesStore, type Profile } from "@/stores/profiles";
 
 const utils = new Utils();
 
@@ -17,20 +18,37 @@ export const resolveNameQuery = async (name: string) => {
   });
 };
 
-export const resolveName = async (name: string) => {
+export const resolveName = async (name: string, force: boolean = false) => {
   if (!isValidName(name)) {
     return null;
   }
 
+  // check cached profiles
+  const cachedProfiles = profilesStore.getState().getProfilesByName(name);
+  if (!force && cachedProfiles.length > 0) {
+    return cachedProfiles;
+  }
+
   const response = await fetch(
-    `${import.meta.env.VITE_API_SERVICE_URL}/api/v1/resolve/name/${window.encodeURIComponent(name)}`,
+    `${import.meta.env.VITE_API_SERVICE_URL}/api/v2/resolve/name/${window.encodeURIComponent(name)}`,
     {
       method: "GET",
     },
   );
-  const data = (await response.json()) as { address: string | null };
 
-  return data.address;
+  if (!response.ok) {
+    return cachedProfiles;
+  }
+
+  const data = (await response.json()) as { profiles: Profile[] };
+
+  if (data.profiles.length > 0) {
+    // cache the profiles
+    profilesStore.getState().addProfiles(data.profiles);
+  }
+
+  // return updated cached profiles
+  return profilesStore.getState().getProfilesByName(name);
 };
 
 export const getInboxIdForAddressQuery = async (
