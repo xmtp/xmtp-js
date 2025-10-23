433aec78f62e74e503245c2e883d926aa5e80362
Benny Neugebauer <mail@bennycode.com>
2025-10-24 00:08:02 +0200

    REM comment

--- sdks/agent-sdk/vitest.config.ts
+++ sdks/agent-sdk/vitest.config.ts
@@ -2,7 +2,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  // @see https://github.com/aleclarson/vite-tsconfig-paths/issues/176
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
