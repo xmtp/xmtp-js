9ef8337a2780e037c0403c226ad07ac166cb3eb3
Ry Racherbaumer <ry@xmtp.com>
2025-10-31 16:25:12 -0500

    Upgrade node types

--- content-types/content-type-remote-attachment/src/encryption/crypto.browser.ts
+++ content-types/content-type-remote-attachment/src/encryption/crypto.browser.ts
@@ -2,4 +2,5 @@
  * DO NOT IMPORT THIS FILE DIRECTLY
  ***********************************************************************************************/
 const crypto = window.crypto;
+export type CryptoKey = typeof window.CryptoKey;
 export default crypto;
