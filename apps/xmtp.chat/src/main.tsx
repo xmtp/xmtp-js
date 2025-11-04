ce9ca5bb5e6c0358c1f78ef622368af7383cbb4a
Ry Racherbaumer <ry@xmtp.com>
2025-11-04 17:42:59 -0600

    Update Sentry config

--- apps/xmtp.chat/src/main.tsx
+++ apps/xmtp.chat/src/main.tsx
@@ -38,6 +38,13 @@ import { queryClient } from "@/helpers/queries";
 
 Sentry.init({
   dsn: "https://ba2f58ad2e3d5fd09cd8aa36038b950f@o4504757119680512.ingest.us.sentry.io/4510308912005120",
+  // ensure no data collection except errors
+  enableLogs: false,
+  profilesSampleRate: 0,
+  replaysOnErrorSampleRate: 0,
+  replaysSessionSampleRate: 0,
+  sendDefaultPii: false,
+  tracesSampleRate: 0,
 });
 
 export const config = createConfig({
