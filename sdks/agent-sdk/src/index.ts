2d450f08f7b04eb9a464f92790564311a55983b5
Fabrizio Guespe <fguespe@gmail.com>
2025-11-22 19:26:34 -0300

    Fix typo in file name

--- sdks/agent-sdk/src/index.ts
+++ sdks/agent-sdk/src/index.ts
@@ -22,3 +22,4 @@ export type * from "@xmtp/node-sdk";
 export { isHexString, validHex } from "@xmtp/node-sdk";
 // Agent SDK
 export * from "./core/index.js";
+export * from "./utils/attachment.js";
