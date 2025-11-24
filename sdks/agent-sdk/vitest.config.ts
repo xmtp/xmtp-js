2f20366037349dc9a1f726ee137fd5177c872157
Ry Racherbaumer <rygine@users.noreply.github.com>
2025-11-24 10:55:28 -0600

    Upgrade developer dependencies (#1560)
    * Upgrade ESLint deps
    
    * Upgrade xmtp.chat deps
    
    * Upgrade API service deps
    
    * Upgrade viem and wagmi
    
    * Upgrade types
    
    * Upgrade vite and vitest deps
    
    * Upgrade dev tools

--- sdks/agent-sdk/vitest.config.ts
+++ sdks/agent-sdk/vitest.config.ts
@@ -1,10 +1,17 @@
+import { defineConfig, mergeConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+const viteConfig = defineConfig({
   plugins: [tsconfigPaths()],
+});
+
+const vitestConfig = defineVitestConfig({
   test: {
     globals: true,
     environment: "node",
   },
 });
+
+export default mergeConfig(viteConfig, vitestConfig);
