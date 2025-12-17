6c2b7ac419ee565776bcaaa3acc01817efee17a6
Benny Neugebauer <mail@bennycode.com>
2025-12-17 01:59:43 +0100

    updated message

--- content-types/content-type-remote-attachment/src/RemoteAttachment.ts
+++ content-types/content-type-remote-attachment/src/RemoteAttachment.ts
@@ -54,7 +54,7 @@ export class RemoteAttachmentCodec
     const response = await fetch(remoteAttachment.url);
     if (!response.ok) {
       throw new Error(
-        `unable to fetch remote attachment at ${remoteAttachment.url}`,
+        `unable to fetch remote attachment at ${remoteAttachment.url}: ${response.status} ${response.statusText}`,
       );
     }
     const payload = new Uint8Array(await response.arrayBuffer());
