f3746db819caf557a9738ed1fca628e0913b9cdf
Benny Neugebauer <mail@bennycode.com>
2025-12-17 01:52:59 +0100

    update demo

--- sdks/agent-sdk/src/demo.ts
+++ sdks/agent-sdk/src/demo.ts
@@ -1,5 +1,6 @@
 import { loadEnvFile } from "node:process";
 import { TextCodec } from "@xmtp/content-type-text";
+import { AttachmentUtil } from "@/utils/AttachmentUtil.js";
 import { Agent, AgentError } from "./core/index.js";
 import { getTestUrl, logDetails } from "./debug/log.js";
 import { isHexString } from "./index.js";
@@ -26,8 +27,12 @@ router.command("/version", async (ctx) => {
 
 agent.use(router.middleware());
 
-agent.on("attachment", (ctx) => {
-  console.log("Got attachment:", ctx.message.content);
+agent.on("attachment", async (ctx) => {
+  const receivedAttachment = await AttachmentUtil.loadRemoteAttachment(
+    ctx.message.content,
+    agent.client,
+  );
+  console.log(`Received attachment: ${receivedAttachment.filename}`);
 });
 
 agent.on("text", (ctx) => {
