2bf593f4d1bfd61cf42fb78d0f73b547a29bd24c
Benny Neugebauer <mail@bennycode.com>
2026-01-12 19:10:25 +0200

    refactor: Simplify signer creation

--- sdks/agent-sdk/src/user/User.ts
+++ sdks/agent-sdk/src/user/User.ts
@@ -1,54 +1,38 @@
-import {
-  IdentifierKind,
-  type HexString,
-  type Identifier,
-  type Signer,
-} from "@xmtp/node-sdk";
+import { IdentifierKind, type HexString, type Signer } from "@xmtp/node-sdk";
 import {
   createWalletClient,
   http,
   toBytes,
   type Chain,
-  type Hex,
   type PrivateKeyAccount,
   type WalletClient,
 } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
-export type User = {
-  key: Hex;
-  account: PrivateKeyAccount;
-  wallet: WalletClient;
-};
-
-export const createUser = (key?: HexString, chain: Chain = sepolia): User => {
+export const createUser = (key?: HexString, chain: Chain = sepolia) => {
   const accountKey = key ?? generatePrivateKey();
   const account = privateKeyToAccount(accountKey);
-  return {
-    key: accountKey,
+  return createWalletClient({
     account,
-    wallet: createWalletClient({
-      account,
-      chain,
-      transport: http(),
-    }),
-  };
+    chain,
+    transport: http(),
+  });
 };
 
-export const createIdentifier = (user: User): Identifier => ({
-  identifier: user.account.address.toLowerCase(),
-  identifierKind: IdentifierKind.Ethereum,
-});
-
-export const createSigner = (user: User): Signer => {
-  const identifier = createIdentifier(user);
+export const createSigner = (
+  wallet: WalletClient & { account: PrivateKeyAccount },
+): Signer => {
+  const identifier = {
+    identifier: wallet.account.address.toLowerCase(),
+    identifierKind: IdentifierKind.Ethereum,
+  };
   return {
     type: "EOA",
     getIdentifier: () => identifier,
     signMessage: async (message: string) => {
-      const signature = await user.wallet.signMessage({
-        account: user.account,
+      const signature = await wallet.signMessage({
+        account: wallet.account!,
         message,
       });
       return toBytes(signature);
