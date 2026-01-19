590d517ed5a1ed4f8e7f8cf2a4b23433dafd9cc0
Ry Racherbaumer <ry@xmtp.com>
2026-01-19 14:49:17 -0600

    Update exports

--- sdks/agent-sdk/src/index.ts
+++ sdks/agent-sdk/src/index.ts
@@ -1,24 +1,3 @@
-export {
-  ConsentEntityType,
-  ConsentState,
-  ConversationType,
-  DeliveryStatus,
-  GroupMember,
-  GroupMembershipState,
-  GroupMessageKind,
-  GroupMetadata,
-  GroupPermissions,
-  GroupPermissionsOptions,
-  IdentifierKind,
-  LogLevel,
-  MetadataField,
-  PermissionLevel,
-  PermissionPolicy,
-  PermissionUpdateType,
-  SignatureRequestHandle,
-  SortDirection,
-} from "@xmtp/node-sdk";
-export type * from "@xmtp/node-sdk";
-export { isHexString, validHex } from "@xmtp/node-sdk";
+export * from "@xmtp/node-sdk";
 // Agent SDK
 export * from "./core/index";
