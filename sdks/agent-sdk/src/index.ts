fc5008e16656365df12484ca8c5527f56eca1131
Ry Racherbaumer <ry@xmtp.com>
2026-01-29 14:11:57 -0600

    Remove unnecessary export

--- sdks/agent-sdk/src/index.ts
+++ sdks/agent-sdk/src/index.ts
@@ -1,5 +1,3 @@
-import { isGroupUpdated } from "@xmtp/node-sdk";
-
 export * from "@xmtp/node-sdk";
 // Agent SDK
 export * from "./core/index";
@@ -7,9 +5,3 @@ export * from "./debug/index";
 export * from "./middleware/index";
 export * from "./user/index";
 export * from "./util/index";
-
-// keep for backwards compatibility
-/** @deprecated use isGroupUpdated instead */
-const isGroupUpdate = isGroupUpdated;
-
-export { isGroupUpdate };
