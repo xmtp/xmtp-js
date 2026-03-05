62cf13a648284597f5b7634b6c83c8ff36fe9c34
Benny Neugebauer <mail@bennycode.com>
2026-03-05 22:23:45 +0100

    simplify design

--- apps/xmtp.chat/src/components/Messages/ReadReceiptContent.tsx
+++ apps/xmtp.chat/src/components/Messages/ReadReceiptContent.tsx
@@ -1,10 +1,7 @@
-import { Group, Text } from "@mantine/core";
+import { Text } from "@mantine/core";
 import { DateLabel } from "@/components/DateLabel";
-import { IdentityBadge } from "@/components/IdentityBadge";
 import { useConversationContext } from "@/contexts/ConversationContext";
 import { nsToDate } from "@/helpers/date";
-import { MEMBER_NO_LONGER_IN_GROUP } from "@/helpers/strings";
-import { getMemberAddress } from "@/helpers/xmtp";
 import { useConversation } from "@/hooks/useConversation";
 
 export type ReadReceiptContentProps = {
@@ -13,23 +10,14 @@ export type ReadReceiptContentProps = {
 };
 
 export const ReadReceiptContent: React.FC<ReadReceiptContentProps> = ({
-  senderInboxId,
   sentAtNs,
 }) => {
   const { conversationId } = useConversationContext();
   const { members } = useConversation(conversationId);
-  const senderMember = members.get(senderInboxId);
   return (
     <>
       <DateLabel date={nsToDate(sentAtNs)} align="center" padding="sm" />
-      <Group gap="4" wrap="wrap" justify="center">
-        <IdentityBadge
-          address={senderMember ? getMemberAddress(senderMember) : ""}
-          displayName={senderInboxId}
-          tooltip={senderMember ? undefined : MEMBER_NO_LONGER_IN_GROUP}
-        />
-        <Text size="sm">sent a read receipt</Text>
-      </Group>
+      <Text size="sm">received read receipt</Text>
     </>
   );
 };
