d22a1f09ce3270c5945e89e7b9b91fe03edb5309
Andrew Plaza <andrew@xmtp.com>
2026-05-15 13:57:09 -0400

    bump to libxmtp nightly 05-15-2026 (#1802)
    update to latest libxmtp nightly

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              corepack
+            ];
+          };
+        };
+    };
+}
